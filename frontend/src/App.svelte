c64b9e0a9256ea0d178a2dfafdbd41b7179569e3
Lauren <iraci.lauren@gmail.com>
2026-07-24 17:35:20 +0200

    Added AuthFlow Support

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,15 +3,38 @@ import TitleBar from "./componets/TitleBar.svelte";
 import {onMount} from "svelte";
 import {WML} from "@wailsio/runtime";
 import HomePage from "./fragments/HomePage.svelte";
+import type {TransitionConfig} from "svelte/transition";
+import AuthFlow from "./fragments/AuthFlow.svelte";
 
 onMount(() => {
     WML.Reload();
 });
 
+const DURATION = typeof location !== 'undefined' && location.search.includes('slow')
+    ? 2000
+    : 320;
+
+function swap(_node: Element, {scale}: {scale: number}): TransitionConfig {
+    return {
+        duration: DURATION,
+        css: (t, u) => `opacity: ${t}; transform: scale(${1 + (scale - 1) * u})`,
+    };
+}
+
+let authenticated = $state(false);
+
 </script>
 <main>
     <TitleBar/>
-    <HomePage/>
+    <div class="page">
+        {#if authenticated}
+            <div class="screen" in:swap={{scale: 1.02}}>
+                <HomePage/>
+            </div>
+        {:else}
+            
+        {/if}
+    </div>
 </main>
 
 <style>
@@ -23,5 +46,19 @@ onMount(() => {
         height: 100vh;
         overflow: hidden;
         background: var(--secondary-bg-color);
+    }
+
+    .page {
+        display: grid;
+        min-height: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    .screen {
+        grid-area: 1 / 1;
+        display: flex;
+        flex-direction: column;
+        min-height: 0;
     }
 </style>
